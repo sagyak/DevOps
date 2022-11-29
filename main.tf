d641bd22d406eecc619f4b120f1dfa2c648ebf81
sagyak <110639277+sagyak@users.noreply.github.com>
2022-11-29 14:36:10 +1100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "sagya"
+
+    workspaces {
+      name = "TestDB"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "DEVOPS" {
+  name    = "DEVOPS"
+  comment = "Database for Snowflake Terraform demo"
+}
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.TESTDB.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
+
+
+
