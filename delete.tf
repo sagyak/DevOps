6307ab6d97af2113d933d191bf5699c90c574294
sagyak <110639277+sagyak@users.noreply.github.com>
2023-03-02 11:01:26 +1100

    Create delete.tf

---
+++ delete.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "sagya"
+
+    workspaces {
+      name = "DevOps"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+terraform destroy -target snowflake_database.devops_db.name
